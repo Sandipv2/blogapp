15fcb0a41b6651334e4365bef5c67e81c77ea73c
sandipv2 <sandiipvarma@gmail.com>
2025-04-11 11:39:56 +0530

    .env setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import conf from './conf/conf.js'
+
+function App() {
+
+  console.log(conf.appwriteUrl)
+
+  return (
+    <>
+      <h1>App</h1>
+    </>
+  )
+}
+
+export default App
